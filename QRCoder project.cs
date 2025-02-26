8247d158e6d68c3e79a0fbca15d1966f342fb795
Patra Badrinath <badrinathyadav5987@gmail.com>
2025-02-27 01:34:20 +0530

    Add files via upload

---
+++ QRCoder project.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QRCoder;
+using WebApplication19.Models;
+using static QRCoder.PayloadGenerator;
+
+namespace WebApplication19.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult CreateQRCode()
+        {
+            //craete view here using Template : Empty  
+            return View(new QRCodeModel());
+        }
+
+        [HttpPost]
+        public ActionResult CreateQRCode(QRCodeModel qrCodeModel)
+        {
+            Payload payload = new Url(qrCodeModel.QRCodeText);
+            QRCodeGenerator codeGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = codeGenerator.CreateQrCode(payload);
+            QRCoder.PngByteQRCode pngByte = new PngByteQRCode(qrCodeData);
+            var QrByte = pngByte.GetGraphic(20);
+            string base64Url = Convert.ToBase64String(QrByte);
+            qrCodeModel.QRImageUrl = "data:image/png;base64," + base64Url;
+            return View("CreateQRCode", qrCodeModel);
+        }
+    }
+}
